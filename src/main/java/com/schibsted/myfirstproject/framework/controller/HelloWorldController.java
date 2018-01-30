bcb69a7fd29d6f8ff01d7d15a870a9dca8184b10
antonio.ibars <juan.ibars@scmspain.com>
2018-01-30 17:51:42 +0100

    Modification of controller

--- src/main/java/com/schibsted/myfirstproject/framework/controller/HelloWorldController.java
+++ src/main/java/com/schibsted/myfirstproject/framework/controller/HelloWorldController.java
@@ -1,15 +1,17 @@
 package com.schibsted.myfirstproject.framework.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/greeting")
 public class HelloWorldController {
-  @GetMapping
-  public String sayHello() {
-    return "Hello world";
+
+  @GetMapping("/{name}")
+  public String sayHello(@PathVariable("name") String name) {
+    return String.format("Hola %s%n", name);
   }
 
 }
