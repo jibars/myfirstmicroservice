d7cd987f9d4f719b5a913028fac6a0e007946780
antonio.ibars <juan.ibars@scmspain.com>
2018-01-30 18:07:10 +0100

    Create repository class

---
+++ src/main/java/com/schibsted/myfirstproject/framework/configuration/RepositoryConfiguration.java
@@ -0,0 +1,14 @@
+package com.schibsted.myfirstproject.framework.configuration;
+
+import com.schibsted.myfirstproject.framework.model.Repository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RepositoryConfiguration {
+
+  @Bean
+  Repository localRepository() {
+    return new Repository();
+  }
+}
---
+++ src/main/java/com/schibsted/myfirstproject/framework/model/exceptions/NotUserException.java
@@ -0,0 +1,7 @@
+package com.schibsted.myfirstproject.framework.model.exceptions;
+
+public class NotUserException extends Exception {
+  public NotUserException(String message) {
+    super(message);
+  }
+}
