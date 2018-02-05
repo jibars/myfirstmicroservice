4c9e4c8008e0ffde1b470d6e349166b72322b15b
antonio.ibars <juan.ibars@scmspain.com>
2018-02-05 19:33:53 +0100

    Create implementation of repository

--- src/main/java/com/schibsted/myfirstproject/framework/configuration/RepositoryConfiguration.java
+++ src/main/java/com/schibsted/myfirstproject/framework/configuration/RepositoryConfiguration.java
@@ -1,5 +1,6 @@
 package com.schibsted.myfirstproject.framework.configuration;
 
+import com.schibsted.myfirstproject.framework.model.InMemoryRepository;
 import com.schibsted.myfirstproject.framework.model.Repository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,6 @@ public class RepositoryConfiguration {
 
   @Bean
   Repository localRepository() {
-    return new Repository();
+    return new InMemoryRepository();
   }
 }
---
+++ src/main/java/com/schibsted/myfirstproject/framework/model/InMemoryRepository.java
@@ -0,0 +1,30 @@
+package com.schibsted.myfirstproject.framework.model;
+
+import com.schibsted.myfirstproject.framework.model.exceptions.NotUserException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class InMemoryRepository implements Repository {
+  private List<User> users = new ArrayList<>();
+
+  @Override
+  public void addUser(User user) {
+    users.add(user);
+  }
+
+  @Override
+  public User getUser(User user) throws NotUserException {
+    Optional<User> storedUser = users.stream().filter(item -> item.equals(user)).findFirst();
+    if (!storedUser.isPresent()) {
+      throw new NotUserException("User not exist");
+    }
+    return storedUser.get();
+  }
+
+  @Override
+  public List<User> getUsers() {
+    return users;
+  }
+}
