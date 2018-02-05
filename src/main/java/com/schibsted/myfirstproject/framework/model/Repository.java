cce6905cd82fa9024eaad747b06f1ac50ce5a8eb
antonio.ibars <juan.ibars@scmspain.com>
2018-02-05 19:31:04 +0100

    Create interface

--- src/main/java/com/schibsted/myfirstproject/framework/model/Repository.java
+++ src/main/java/com/schibsted/myfirstproject/framework/model/Repository.java
@@ -2,26 +2,10 @@ package com.schibsted.myfirstproject.framework.model;
 
 import com.schibsted.myfirstproject.framework.model.exceptions.NotUserException;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-public class Repository {
-  private List<User> users = new ArrayList<>();
-
-  public void addUser(User user) {
-    users.add(user);
-  }
-
-  public User getUser(User user) throws NotUserException {
-    Optional<User> storedUser = users.stream().filter(item -> item.equals(user)).findFirst();
-    if (!storedUser.isPresent()) {
-      throw new NotUserException("User not exist");
-    }
-    return storedUser.get();
-  }
-
-  public List<User> getUsers() {
-    return users;
-  }
+public interface Repository {
+  void addUser(User user);
+  User getUser(User user) throws NotUserException;
+  List<User> getUsers();
 }
